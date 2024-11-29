fafffb1bbbb18a670d0ec328bf4b65fd4274bd69
DARKY-LAB <nk1010057@gmail.com>
2024-11-29 10:46:27 +0530

    Update file_rename.py

--- plugins/file_rename.py
+++ plugins/file_rename.py
@@ -223,7 +223,7 @@ async def auto_rename_files(client, message):
         if _bool_metadata:
             metadata = await codeflixbots.get_metadata_code(user_id)
             if metadata:
-                cmd = f'ffmpeg -i "{renamed_file_path}"  -map 0 -c:s copy -c:a copy -c:v copy -metadata title="{metadata}" -metadata author="{metadata}" -metadata:s:s title="{metadata}" -metadata:s:a title="{metadata}" -metadata:s:v title="{metadata}"  "{metadata_file_path}"'
+                cmd = f'ffmpeg -i "{renamed_file_path}"  -map 0 -c:s copy -c:a copy -c:v copy -metadata title="GenAnimeOfc [t.me/GenAnimeOfc]" -metadata author="DARKXSIDE78" -metadata encoded_by="GenAnimeOfc" -metadata:s:s title="{metadata}" -metadata:s:a title="{metadata}" -metadata:s:v title="{metadata}"  "{metadata_file_path}"'
                 try:
                     process = await asyncio.create_subprocess_shell(
                         cmd,
